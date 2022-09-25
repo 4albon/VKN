4c50f07b8543de769f8ed0642f866442635370c6
4albon <113344076+4albon@users.noreply.github.com>
2022-09-25 18:18:14 +0200

    Add files via upload

---
+++ lab5.1.py
@@ -0,0 +1,16 @@
+from math import * 
+
+x = float(input("Enter x:"))
+
+if  x <= -0.5:
+    y= abs(-x**2 + (2*x)**-x)
+    print("first")
+
+elif x> -0.5 and x<5:
+    y= (cos(x) + sin(2*x))
+    print("second")
+
+else:
+    y= -9 + log(x) + sqrt(x)
+    print("third")
+print ("y =" ,y)
