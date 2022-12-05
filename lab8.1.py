f5670aba562a52f491e85eda4150fccd87eed18b
4albon <113344076+4albon@users.noreply.github.com>
2022-12-05 14:25:22 +0100

    Add files via upload

--- lab8.1.py
+++ lab8.1.py
@@ -1,30 +1,40 @@
 from array import *
 import random
 
+
+def bubble_sort(nums):
+    # Устанавливаем swapped в True, чтобы цикл запустился хотя бы один раз
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
 def main():
 # task 1
-    arr = array('i',[])
+    arr = array('f',[])
 
     n = 4
 
-    arr = [pow((1/2), i) for i in range(n)]
-    # for i in range(n):
-    #     arr.append(pow((1/2), i))
-
-    # arr = [random.randint(-1,3) for i in range(n)]
+    for i in range(n):
+        arr.append(pow((1/2), i))
 
     for i in range(n):
         arr.append(random.randint(-1,3))
 
-    arr.sort(reverse=True)
-
+    bubble_sort(arr)
+    arr.reverse()
+    print(arr)
+    
     arr.pop(0)
     arr.pop(0)
     arr.pop(-1)
     arr.pop(-1)
-    print(arr) 
 
+    print(arr)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- lab8.2.py
+++ lab8.2.py
@@ -1,14 +1,10 @@
 import numpy as np
-import numpy.random as rand
-
-
-# task 2
 
 q = np.random.randint(0,30,(3,3))
+print(q)
 r = q * 2
 r = r + q
 print(r)
 
 res = r[0][0] * r[0][2] * r[2][0] * r[2][2]
-
 print(res)
